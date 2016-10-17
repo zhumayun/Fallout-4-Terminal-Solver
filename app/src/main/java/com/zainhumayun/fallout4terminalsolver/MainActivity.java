651952f048ce5fc6e89c8f699e41f8f55b23a0d4
Zain Humayun <zain1019@hotmail.com>
2016-10-16 20:32:14 -0400

    Initial commit

---
+++ app/src/main/java/com/zainhumayun/fallout4terminalsolver/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zainhumayun.fallout4terminalsolver;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // hide toolbar
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null)
+            actionBar.hide();
+    }
+}
